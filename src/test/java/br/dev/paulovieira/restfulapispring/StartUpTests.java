ac4798b0a18d615202a497fd5bd9672b4f00aed6
Junior Vieira <junior.vieira.1990@gmail.com>
2022-11-10 13:51:15 -0300

    :tada: Commit Inicial

---
+++ src/test/java/br/dev/paulovieira/restfulapispring/StartUpTests.java
@@ -0,0 +1,13 @@
+package br.dev.paulovieira.restfulapispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartUpTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
