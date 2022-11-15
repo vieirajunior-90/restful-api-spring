d4f7312aaad2417161de7c1eda6c8b48b8c9523f
Junior Vieira <junior.vieira.1990@gmail.com>
2022-11-15 14:41:33 -0300

    :pencil: sql: Script para popular a tabela person

---
+++ src/main/resources/db/migration/V2__populate_table_person.sql
@@ -0,0 +1,5 @@
+insert into person (id, first_name, last_name, address, gender)values
+(1, 'John', 'Marston', 'Valentine', 'Male'),
+(2, 'Arthur', 'Morgan', 'Saint Denis', 'Male'),
+(3, 'Dutch', 'Van der Linde', 'West Elizabeth', 'Male');
+
