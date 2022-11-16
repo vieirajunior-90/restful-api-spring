eab259dfd68c5634feb2cfc734a0fafaf6649987
Junior Vieira <junior.vieira.1990@gmail.com>
2022-11-15 21:03:27 -0300

    :recycle: refactor: Métodos modificados para receber DTO

--- src/main/java/br/dev/paulovieira/restfulapispring/service/PersonService.java
+++ src/main/java/br/dev/paulovieira/restfulapispring/service/PersonService.java
@@ -1,14 +1,13 @@
 package br.dev.paulovieira.restfulapispring.service;
 
 import br.dev.paulovieira.restfulapispring.dto.*;
-import br.dev.paulovieira.restfulapispring.model.*;
 import org.springframework.data.domain.*;
 
 public interface PersonService {
 
-    Person findById(Long id);
-    Page<Person> findAll(Pageable pageable);
-    Person save(PersonDto personDto);
-    Person update(PersonDto personDto);
+    PersonDto findById(Long id);
+    Page<PersonDto> findAll(Pageable pageable);
+    PersonDto save(PersonDto personDto);
+    PersonDto update(PersonDto personDto);
     void deleteById(Long id);
 }
