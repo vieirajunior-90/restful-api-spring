20fd8322d79c9bcab92786ab3df54f2b6920e2fa
Junior Vieira <junior.vieira.1990@gmail.com>
2022-11-19 22:45:08 -0300

    :sparkles: feat: Ajustes para o CORS

--- src/main/java/br/dev/paulovieira/restfulapispring/config/WebConfig.java
+++ src/main/java/br/dev/paulovieira/restfulapispring/config/WebConfig.java
@@ -1,5 +1,6 @@
 package br.dev.paulovieira.restfulapispring.config;
 
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.context.annotation.*;
 import org.springframework.http.*;
 import org.springframework.web.servlet.config.annotation.*;
@@ -7,6 +8,20 @@ import org.springframework.web.servlet.config.annotation.*;
 @Configuration
 @Profile({"dev", "test"})
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(), HttpMethod.DELETE.name())
+                .allowCredentials(true);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
