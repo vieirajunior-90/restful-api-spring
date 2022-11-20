e7fcd64eecb3419d2aeb3f1ca07c4a67d889edc9
Junior Vieira <junior.vieira.1990@gmail.com>
2022-11-19 22:44:52 -0300

    :sparkles: feat: Novas constantes para os testes de integração

--- src/test/java/br/dev/paulovieira/restfulapispring/config/TestConfigs.java
+++ src/test/java/br/dev/paulovieira/restfulapispring/config/TestConfigs.java
@@ -3,4 +3,8 @@ package br.dev.paulovieira.restfulapispring.config;
 public class TestConfigs {
 
     public static final int SERVER_PORT = 8090;
+    public static final String HEADER_PARAM_ORIGIN = "Origin";
+    public static final String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    public static final String CONTENT_TYPE_JSON = "application/json";
+    public static final String HEADER_PARAM_ORIGIN_NOT_ALLOWED = "http://localhost:9000";
 }
