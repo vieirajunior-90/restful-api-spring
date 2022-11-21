af7816f1226e617e49e27a0a6467c3bf017b206c
Junior Vieira <junior.vieira.1990@gmail.com>
2022-11-21 00:19:53 -0300

    :heavy_plus_sign: build: Dependências adicionadas: Spring Security | Java JWT

---
+++ src/main/resources/db/migration/V11__update_table_users_password_to_bcrypt.sql
@@ -0,0 +1,9 @@
+﻿UPDATE users
+SET password = '$2y$10$lpoBViCmlJftRbT2PL7XQOisnWuRf8rhP0mTZiLbI1nqVq7FhfCcO'
+WHERE user_name = 'john';
+
+UPDATE users
+SET password = '$2y$10$/kPCUCHFI5L8/uwWaGJOSuGlutaZHPLo.GS/SUY..WGHl9wBLmAt.'
+WHERE user_name = 'carl';
+
+# The password is now stored as a bcrypt hash. The hash is 60 characters long, and it is stored in the password column.
